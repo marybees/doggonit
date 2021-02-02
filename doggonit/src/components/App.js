cbe3d3ed065487427a6f822ddab79ab8e5748051
Mary Baker <meb601@gmail.com>
2021-02-02 09:45:10 -0500

    Install Reactstrap and import into project.

---
+++ doggonit/src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import { Switch, Route, Redirect } from 'react-router-dom';
+import { Button } from 'reactstrap';
+
+function App() {
+  return (
+    <div>
+      <Button>Doggonit!</Button>
+    </div>
+  );
+}
+
+export default App;
