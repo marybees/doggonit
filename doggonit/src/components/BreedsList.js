386467194218c6917a23ad42b89b2f2d24564240
Mary Baker <meb601@gmail.com>
2021-02-08 17:25:31 -0500

    Update Reactstrap component styling in BreedList.

--- doggonit/src/components/BreedsList.js
+++ doggonit/src/components/BreedsList.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { Alert } from "reactstrap";
+import { Input, Alert, List, Jumbotron, Button } from "reactstrap";
 
 const BreedsList = ( { dogBreeds } ) => {
     const [search, setSearch] = useState("");
@@ -10,23 +10,26 @@ const BreedsList = ( { dogBreeds } ) => {
     const filteredBreeds = dogBreedsArr.filter(dogBreed => {
         return dogBreed.toLowerCase().includes(search.toLowerCase());
     });
-    if (filteredBreeds.length > 0) {
-        return (
-            <div>
-                <input type="text" placeholder="Search for a breed" onChange={ e => setSearch(e.target.value)}/>
+
+    return (
+        <div>
+            <Jumbotron className="jumbotron">
+                <h1 className="display-3">Dogs on Things by Breed</h1>
+                <p className="lead">Search for your favorite dog breed.</p>
+                <hr className="my-2" />
+                <p>Select a dog breed below to see what they look like!</p>
+                <Input type="text" placeholder="Search for a breed" onChange={ e => setSearch(e.target.value)}/>
+                <p className="lead">
+                </p>
+            </Jumbotron>
+            <div className="dog-list-container">
                 {filteredBreeds.map((dogBreed) => {
-                    return <Link><ul>{dogBreed}</ul></Link>
+                    return <Link><List style={{padding: "0"}}>{dogBreed}</List></Link>
                 })}
+                <Alert color="warning"><span style ={{ fontWeight: "bold" }}>Doggonit!</span> We can't find that dog breed.</Alert>
             </div>
-        )
-    } else {
-        return (
-            <div>
-                <input type="text" placeholder="Search for a breed" onChange={ e => setSearch(e.target.value)}/>
-                <Alert color="warning"><span style ={{ fontWeight: "bold" }}>Doggonit!</span> We can't find the dog breed you're looking for.</Alert>
-            </div>
-        )
-    }
+        </div>
+    )
 };
 
 export default BreedsList;
