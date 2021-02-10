421216d06090631c8e424b664e8cd3e37043b0a0
Mary Baker <meb601@gmail.com>
2021-02-09 19:32:33 -0500

    Update CardImg with position relative and overflow hidden.

--- src/components/DogCard.js
+++ src/components/DogCard.js
@@ -9,7 +9,7 @@ const DogCard = (props) => {
     return (
         <div>
             <Card className="dog-image-card">
-                <CardImg style={{ width: "200px", height: "200px" }} src={props.url} alt="dog"/>
+                <CardImg style={{ width: "200px", height: "200px", position: "relative", overflow: "hidden"}} src={props.url} alt="dog"/>
             </Card>
         </div>
     );
